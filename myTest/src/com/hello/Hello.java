e8f74590fe965576e0738a0a2132bd9b6683851c
zycupc <zycupc@163.com>
2019-05-26 13:45:17 +0800

    create project for test git

---
+++ myTest/src/com/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.hello;
+
+public class Hello {
+	/*public static void main(String[] args) {
+		System.out.println("helloworld");
+	}*/
+	public void fun(){
+		System.out.println("我爱你");
+	}
+}
